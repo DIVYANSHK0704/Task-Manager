4032bb0a524ee99fdf01e4a73a84dd07a6d843ff
DIVYANSHK0704 <divyanshk324@gmail.com>
2026-03-31 02:52:52 +0530

    full project commit

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+const app = express();
+
+//Middleware to handel cors
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+//Middleware
+app.use(express.json());
+
+//connect database
+connectDB();
+
+//Routes
+//app.use("/api/auth",authRoutes);
+//app.use("/api/users",userRoutes);
+//app.use("/api/task",taskRoutes);
+//app.use("/api/report",reportRoutes);
+
+//Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('server running on port 5000'))
