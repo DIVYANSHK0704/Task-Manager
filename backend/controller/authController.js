10e439c52f64c53918c28095bcc6e50b07b87f19
DIVYANSHK0704 <divyanshk324@gmail.com>
2026-04-01 01:46:59 +0530

    full project commit

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { adminOnly } = require("../middleware/authMiddleware");
 
 //Generate jwt token
 const generateToken = (userId) => {
@@ -10,21 +11,100 @@ const generateToken = (userId) => {
 //@desc register a new user
 //@route post/api/auth/login
 //@access public
-const registerUser =async (req,res) => {};
+const registerUser =async (req,res) => {
+    try {
+        const {name,email,password,profileImageUrl,adminInviteToken} = req.body;
+
+    //check if user already exists
+    const userExists = await User.findOne({email});
+    if(userExists){
+        return res.status(400).json({message: "user already exists"})
+    }
+
+    //determine  user role: admin if correct token is provided, otherwise member
+    let role = "member";
+    if(
+        adminInviteToken &&
+        adminInviteToken == process.env.ADMIN_INVITE_TOKEN
+     ) {
+        role ="admin";
+    }
+
+    //hash password 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+
+    //create new user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+        profileImageUrl,
+        role
+    });
+
+    //return user data jwt
+    res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        profileImageUrl: user.profileImageUrl,
+        token: generateToken(user._id),
+    });
+    
+    } catch (error) {
+        res.status(500).json({message:"server error", error: error.message});
+    }
+};
 
 //@desc login user
 //@route post/api/auth/login
 //@access public
-const loginUser =async (req,res) => {};
+const loginUser =async (req,res) => {   try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+    return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+    // Return user data with JWT
+    res.json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    profileImageUrl: user.profileImageUrl,
+    token: generateToken(user._id),
+    });
+
+    } catch (error) {
+        res.status(500).json({message:"server error", error: error.message});
+    }};
 
 //@desc get user profile
 //@route post/api/auth/login
 //@access private(reqire jwt)
-const getUserProfile =async (req,res) => {};
+const getUserProfile =async (req,res) => {   try {
+        
+    } catch (error) {
+        res.status(500).json({message:"server error", error: error.message});
+    }};
 
 //@desc update user profile
 //@route post/api/auth/login
 //@access private(require jwt)
-const updateUserProfile =async (req,res) => {};
+const updateUserProfile =async (req,res) => {   try {
+        
+    } catch (error) {
+        res.status(500).json({message:"server error", error: error.message});
+    }};
 
 module.exports = {registerUser,loginUser,getUserProfile,updateUserProfile};
--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-//Middleware to handel cors
+console.log("🚀 Starting server...");
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-//Middleware
+// Middleware
+app.use(cors());
 app.use(express.json());
 
-//connect database
+
+// Connect DB
 connectDB();
 
-//Routes
-app.use("/api/auth",authRoutes);
+// Routes
+app.use("/api/auth", authRoutes);
 //app.use("/api/users",userRoutes);
 //app.use("/api/task",taskRoutes);
 //app.use("/api/report",reportRoutes);
 
-//Start server
+// Start server (CRITICAL FIX)
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('server running on port 5000'))
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
